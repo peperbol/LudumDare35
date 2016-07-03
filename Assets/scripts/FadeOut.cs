63bb9c87d6b5b8ea2b7d55fb62423aa3f20adb49
Pepijn Willekens <pepijnwillekens@gmail.com>
2016-07-04 00:57:21 +0200

    purle water, start character physics

---
+++ Assets/scripts/FadeOut.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeOut : MonoBehaviour {
+    public float time = 1;
+    SpriteRenderer sr;
+    float timer;
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+
+        timer = time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer < 0) Destroy(gameObject);
+        sr.color = sr.color.SetA(time / timer);
+	}
+}
---
+++ Assets/scripts/RaintHit.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaintHit : MonoBehaviour
+{
+    ParticleSystem ps;
+    ParticleCollisionEvent[] particleEvent;
+    public GameObject dripPrefab;
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        particleEvent = new ParticleCollisionEvent[16];
+    }
+
+    public void OnParticleCollision(GameObject other)
+    {
+        var safelength = ps.GetSafeCollisionEventSize();
+        if(safelength> particleEvent.Length)
+        {
+            particleEvent = new ParticleCollisionEvent[safelength];
+        }
+        var numCollisionEvents = ps.GetCollisionEvents(other, particleEvent);
+        for (int i = 0; i < numCollisionEvents; i++)
+        {
+            Instantiate(dripPrefab, particleEvent[i].intersection, Quaternion.Euler(90,0,0));
+        }
+    }
+}
